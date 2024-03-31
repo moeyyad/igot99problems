522c87665583481b484882a06d16e4ee74650cf2
Moeyyad Qureshi <moeyyadqureshi@gmail.com>
2024-03-31 15:50:01 -0400

    rough draft

---
+++ src/content/post/episode-1.md
@@ -0,0 +1,41 @@
+---
+title: "Dating Apps are Broken"
+description: "...In more ways than one. While dating apps have revolutionized the way that couples meet, they introduce many large problems in their wake"
+publishDate: "31 Mar 2024"
+tags: ["episode", "dating"]
+---
+
+## Introduction to this Problem
+test
+
+1. Subproblem
+2. Subproblem
+3. Subproblem
+4. Subproblem
+5. Subproblem
+
+## Problem Analysis
+
+### Subproblem 1
+test
+
+### Subproblem 2
+test
+
+### Subproblem 3
+test
+
+### Subproblem 4
+test
+
+### Subproblem 5
+test
+
+## Past Failures
+test
+
+## Difficulties
+test
+
+## How To Get Started
+test
--- src/site.config.ts
+++ src/site.config.ts
@@ -3,9 +3,9 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Moeyyad Qureshi",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "I Got 99 Problems",
 	// Meta property used as the default description meta property
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18
@@ -37,9 +37,9 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		path: "/igot99problems/about/",
 	},
 	{
-		title: "Blog",
+		title: "Episodes",
 		path: "/igot99problems/posts/",
-	},
+	}
 ];
 
 // https://expressive-code.com/reference/configuration/
